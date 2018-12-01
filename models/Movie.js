192ced2dc25aeb532b78269ebecba427e46d0a70
Fredy Sanchez <00082817@uca.edu.sv>
2018-12-01 14:18:57 -0600

    Modelo de pelicula

--- models/Movie.js
+++ models/Movie.js
@@ -1,2 +1,16 @@
+var mongoose = require('mongoose');
 
+var MovieSchema = new mongoose.Schema({
+    name: {
+        type: String,
+        unique: true,
+        index: true
+    },
+    gender: {
+        type: String,
+        required: true
+    },
+    year: Date
+});
 
+module.exports = mongoose.model('Movie', MovieSchema);

ce05170a85b7cc8827a3832ad269c9b2295ea88f
Fredy Sanchez <00082817@uca.edu.sv>
2018-12-01 15:04:30 -0600

    Controlador empezado

---
+++ controllers/MovieController.js
@@ -0,0 +1,102 @@
+var mongoose = require('mongoose');
+movieModel = require('../models/Movie');
+
+let controller = {};
+
+controller.getAll = function(req, res){
+    movieModel.find({}, function(err, users){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                users
+            });
+        }
+    });
+};
+
+controller.getOne = function(req, res){
+    movieModel.findOne({_id: req.params.id}, function(err, user){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                user
+            });
+        }
+    });
+};
+
+controller.update = function(req, res){
+    let update = {
+        name: req.body.name,
+        gender: req.body.gender,
+        year: req.body.year
+    }
+    movieModel.findByIdAndUpdate(req.params.id, update,function(err, old){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                old,
+                update
+            });
+        }
+    })
+};
+
+controller.insert = function(req, res){
+    let newMovie = new movieModel({
+        name: req.body.name,
+        gender: req.body.gender,
+        year: req.body.year
+    });
+    newMovie.save(function(err, inserted){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                insert
+            });
+        }
+    });
+};
+
+controller.delete = function(req, res){
+    movieModel.findByIdAndRemove(req.params.id, function(err, eliminado){
+        if(err){
+            res.status(500);
+            res.json({
+                ok: false,
+                err
+            });
+        } else {
+            res.json({
+                ok: true,
+                eliminado
+            });
+        }
+    });
+};
+
+module.exports = controller;
